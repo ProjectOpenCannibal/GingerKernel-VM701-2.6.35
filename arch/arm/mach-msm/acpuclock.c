8ba99d2dd86eb8c3e9ea03e4bcbe9bcab3331054
Drew Walton <drewwalton19216801@gmail.com>
2011-11-16 17:02:37 -0700

    Add initial overclock code

--- arch/arm/mach-msm/acpuclock.c
+++ arch/arm/mach-msm/acpuclock.c
@@ -449,6 +449,13 @@ static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s)
 		writel(reg_clksel, A11S_CLK_SEL_ADDR);
 	}
 
+	// Perform overclocking if requested
+	if(hunt_s->pll==0 && hunt_s->a11clk_khz>800000) {
+		// Change the speed of PLL0
+		writel(hunt_s->a11clk_khz/19200, PLLn_L_VAL(0));
+		udelay(50);
+	}
+
 	/* Program clock source and divider */
 	reg_clkctl = readl(A11S_CLK_CNTL_ADDR);
 	reg_clkctl &= ~(0xFF << (8 * src_sel));
@@ -459,6 +466,13 @@ static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s)
 	/* Program clock source selection */
 	reg_clksel ^= 1;
 	writel(reg_clksel, A11S_CLK_SEL_ADDR);
+
+	// Recover from overclocking
+	if(hunt_s->pll==0 && hunt_s->a11clk_khz<=800000) {
+		// Restore the speed of PLL0
+		writel(50, PLLn_L_VAL(0));
+		udelay(50);
+	}
 
 	/*
 	 * If the new clock divider is lower than the previous, then
